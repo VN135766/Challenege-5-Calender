6601cf8f5534994b7522b0fce3bef8cbf0f02d21
Victor Nunez <VN135766@gmail.com>
2022-05-05 16:24:37 -0400

    added save button and text input

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,5 +1,12 @@
 function today () {
     var todayDate = moment().format('MMMM Do YYYY, h:mm a');
-$("#currentDay").text(todayDate)
+$("#currentDay").html(todayDate)
 }
 today()
+
+$(".saveBtn").on("click", function(event) {
+var text = $(event.target).siblings(".description").val();
+
+
+
+})
