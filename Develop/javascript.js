2d484733e2590746955043e017da7741ed1fbcbd
Victor Nunez <VN135766@gmail.com>
2022-05-05 18:11:25 -0400

    added local storage function

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,12 +1,27 @@
-function today () {
+function today() {
     var todayDate = moment().format('MMMM Do YYYY, h:mm a');
-$("#currentDay").html(todayDate)
+    $("#currentDay").html(todayDate);
 }
-today()
+today();
 
-$(".saveBtn").on("click", function(event) {
-var text = $(event.target).siblings(".description").val();
+var saveBtn = $(".saveBtn");
 
+saveBtn.on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var index = $(this).siblings(".hour").text();
+    console.log(text);
+    localStorage.setItem(index, text);
+}
+);
 
-
-})
+function displayPlanner() {
+    $(".hour").each(function () {
+        var currentHourOnPlanner = $(this).text();
+        var currentTask = localStorage.getItem(currentHourOnPlanner);
+        if(currentTask != "" ) {
+            $(this).siblings(".description").val(currentTask);
+        }
+    }
+    )
+}
+displayPlanner();
